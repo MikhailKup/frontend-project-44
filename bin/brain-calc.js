dfe36801f8eabbe6508cbd4a257693a50d845a78
Mikhail <mifffa.mk@gmail.com>
2023-10-12 08:10:04 +0500

    Step 9. Fix brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line import/named
-import { playBrainCalc } from '../src/games/calc.js';
+import { playBrainCalc } from "../src/games/calc.js";
 
-import { setGameRules } from '../src/index.js';
+import { setGameRules } from "../src/index.js";
 
-const taskOfCalc = 'What is the result of the expression?';
+const taskOfCalc = "What is the result of the expression?";
 setGameRules(taskOfCalc, playBrainCalc);
